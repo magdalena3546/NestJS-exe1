0447f28713526be5cff5165ff206aee10dadb228
Magdalena <magdalenamalek52@gmail.comgit statusgit status git config --global user.email magdalenamalek52@gmail.com>
2022-11-27 14:34:15 +0100

    fix bug

--- src/ormconfig.ts
+++ src/ormconfig.ts
@@ -1,10 +1,12 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+require('dotenv').config();
 export const typeOrmConfig: TypeOrmModuleOptions = {
   type: 'mysql',
   host: 'localhost',
   port: 3306,
   username: 'root',
-  password: 'Shuri3546#',
+  password: process.env.PASSWORD,
   database: 'shop',
   entities: [__dirname + '/**/*.entity{.ts,.js}'],
   synchronize: true,
